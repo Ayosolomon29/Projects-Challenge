f0750ca35d5aeadd4fc839bdb95a14eefe0da02c
Ayobami-S-O <damilolaode01@gmail.com>
2022-07-08 19:53:21 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 
-var birthyear = prompt("What year were you born ... Dear friend?")
+
+const ageInDays = () => {
+   var birthYear =  prompt("What year were your born?");
+   var ageDays = (2022 - birthYear) * 365
+   console.log(ageDays)
+   document.getElementById("flex-box-result").innerHTML = `You are ${ageDays} years old in days`
+}
+
+function reset(){
+    document.getElementById("flex-box-result").remove()
+}
